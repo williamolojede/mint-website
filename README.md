dd447dcc15f16fef8d2dcba1339b7ae336152f8e
William I. Olojede <ilerioluwase97@gmail.com>
2017-06-16 09:13:01 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Mint Website
+A react web app for the the [made in nigeria tech repo](https://github.com/acekyd/made-in-nigeria)
+
+## features 
+1. Admin mode for managing the list
+2. A user mode to view/search the list
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Something cool<span role="img" aria-label="cool">😎</span> is coming!!!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
